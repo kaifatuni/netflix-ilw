2abc9d5c9d5d122d0e7b10886efe8da69f3d5f8f
unknown <kaif.201mn033@nitk.edu.in>
2022-11-08 13:34:29 +0530

    Revert "Firebase Change"
    This reverts commit 22cabb13b80263a5b7dd82aaabd6b8e4531cb131.

--- firebase.ts
+++ firebase.ts
@@ -9,13 +9,13 @@ import { getAuth } from 'firebase/auth'
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyA9sELYLJbEyl1Fz8luKrCsc3U0RFTakzs",
-  authDomain: "netflix-ilw-561a4.firebaseapp.com",
-  projectId: "netflix-ilw-561a4",
-  storageBucket: "netflix-ilw-561a4.appspot.com",
-  messagingSenderId: "1082514915095",
-  appId: "1:1082514915095:web:39cf78666102cd0dd18152",
-  measurementId: "G-X7CFQ4VPKN"
+  apiKey: 'AIzaSyBuu0YHXOcgmqWLZl-fiMn6lG2wUhdPg8k',
+  authDomain: 'next-firebase-stripe-39bf8.firebaseapp.com',
+  databaseURL: 'https://next-firebase-stripe-39bf8-default-rtdb.firebaseio.com',
+  projectId: 'next-firebase-stripe-39bf8',
+  storageBucket: 'next-firebase-stripe-39bf8.appspot.com',
+  messagingSenderId: '777709922250',
+  appId: '1:777709922250:web:4500ee09dca93e1406d133',
 }
 
 // Initialize Firebase
