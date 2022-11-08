6d1f6dcc08b552f81d00ea4f06636f618b25d5d3
unknown <kaif.201mn033@nitk.edu.in>
2022-11-08 22:01:39 +0530

    Update Plans.tsx

--- components/Plans.tsx
+++ components/Plans.tsx
@@ -34,13 +34,13 @@ function Plans({ products }: Props) {
       </Head>
       <header className="border-b border-white/10 bg-[#141414]">
         <Link href="/">
-          <img
+          {/* <img
             src="https://rb.gy/ulxxee"
             alt="Netflix"
             width={150}
             height={90}
             className="cursor-pointer object-contain"
-          />
+          /> */}
         </Link>
         <button
           className="text-lg font-medium hover:underline"
